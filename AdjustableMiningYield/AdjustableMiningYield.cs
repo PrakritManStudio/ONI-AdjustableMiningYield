77a790707f1de1308c0b0b6f8e373f5fae98a8cc
Prakr <Prakr@PRAKRITM_PC>
2023-06-06 18:56:32 +0700

    Add project files.

---
+++ AdjustableMiningYield/AdjustableMiningYield.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+using KMod;
+using PeterHan.PLib.Core;
+using PeterHan.PLib.Database;
+using PeterHan.PLib.Options;
+
+namespace AdjustableMiningYield
+{
+    public class AdjustableMiningYield : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            PUtil.InitLibrary();
+            new PLocalization().Register();
+            var options = new POptions();
+            options.RegisterOptions(this, typeof(AdjustableMiningYieldConfig));
+        }
+
+        [HarmonyPatch(typeof(WorldDamage), "OnDigComplete")]
+        internal class AdjustableMiningYield_WorldDamage_OnDigComplete
+        {
+            
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var config = POptions.ReadSettings<AdjustableMiningYieldConfig>() ?? new AdjustableMiningYieldConfig();
+                foreach (CodeInstruction instruction in instructions.ToList())
+                {
+                    if (IsInstructionLoadConstant(instruction, 0.5f))
+                    {
+                        Debug.Log("=== Transpiler applied ===");
+
+                        float yValue;
+                        switch (config.YieldValue)
+                        {
+                            case DuplicantSightRange.V35:
+                                yValue = 0.35f;
+                                break;
+                            case DuplicantSightRange.V50:
+                                yValue = 0.5f;
+                                break;
+                            case DuplicantSightRange.V60:
+                                yValue = 0.6f;
+                                break;
+                            case DuplicantSightRange.V75:
+                                yValue = 0.75f;
+                                break;
+                            case DuplicantSightRange.V100:
+                                yValue = 1.0f;
+                                break;
+                            case DuplicantSightRange.V125:
+                                yValue = 1.25f;
+                                break;
+                            case DuplicantSightRange.V150:
+                                yValue = 1.5f;
+                                break;
+                            default:
+                                yValue = 0.5f;
+                                break;
+
+                        }
+                        instruction.operand = yValue;
+                    }
+
+                    yield return instruction;
+                }
+            }
+
+            private static bool IsInstructionLoadConstant(CodeInstruction instruction, float value)
+            {
+                return instruction.opcode == OpCodes.Ldc_R4 && (float)instruction.operand == value;
+            }
+        }
+    }
+}
---
+++ AdjustableMiningYield/AdjustableMiningYieldConfig.cs
@@ -0,0 +1,29 @@
+﻿using PeterHan.PLib.Options;
+
+namespace AdjustableMiningYield
+{
+    public enum DuplicantSightRange
+    {
+        [Option("35%")]
+        V35 = 35,
+        [Option("50% Default")]
+        V50 = 50,
+        [Option("60%")]
+        V60 = 60,
+        [Option("75%")]
+        V75 = 75,
+        [Option("100% Full Miner Yield")]
+        V100 = 100,
+        [Option("125%")]
+        V125 = 125,
+        [Option("150%")]
+        V150 = 150
+    }
+
+    public class AdjustableMiningYieldConfig
+    {
+        [Option("Miner Results",
+            "Volume obtained from mining")]
+        public DuplicantSightRange YieldValue { get; set; } = DuplicantSightRange.V100;
+    }
+}
